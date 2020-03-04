99d381aa54b4bf93b18ec9333a16d67c7a25340a
Kaic Melo <kaicmelosantos@gmail.com>
2020-03-04 13:42:57 -0300

    inicio da disposição do layout de trasfer , começando a pagina home do sistema

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -11,51 +11,71 @@ class Dashboard extends StatelessWidget{
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme
-                  .of(context)
-                  .primaryColor,
-              child: InkWell(
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => ContactsList(),
-                  )
-                  );
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png'),
             ),
-          )
+          Row(
+            children: <Widget>[
+              _FeatureItem('Transfer',Icons.monetization_on,onClick: (){}), 
+              _FeatureItem('Transaction Feed',Icons.description,onClick: (){}), 
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+
+  final String name ;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem(this.name, this.icon, {@required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return         
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme
+            .of(context)
+            .primaryColor,
+        child: InkWell(
+          onTap: (){
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => ContactsList(),
+            )
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
